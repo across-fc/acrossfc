c24a5b79abd889d665ff10123f6935a865223276
kohenchia <kohenchia@gmail.com>
2024-03-20 21:08:50 -0700

    Fixed clear_order

--- ffxiv_clear_rates/reports/clear_order.py
+++ ffxiv_clear_rates/reports/clear_order.py
@@ -8,23 +8,22 @@ from tabulate import tabulate
 
 # Local
 from ffxiv_clear_rates.database import Database
-from ffxiv_clear_rates.model import TrackedEncounter
 from .report import Report
 
 
 class ClearOrder(Report):
-    def __init__(self, database: Database, encounters: List[TrackedEncounter]):
+    def __init__(self, database: Database, encounter_names: List[str]):
         clear_order = database.get_clear_order()
 
         buffer = StringIO()
-        for i, encounter in enumerate(encounters):
+        for i, encounter_name in enumerate(encounter_names):
             if i > 0:
                 buffer.write("\n\n")
 
-            buffer.write(f"[{encounter.name}]")
+            buffer.write(f"[{encounter_name}]")
             buffer.write("\n\n")
             table = []
-            for i, datapoint in enumerate(clear_order[encounter.name]):
+            for i, datapoint in enumerate(clear_order[encounter_name]):
                 table.append(
                     [
                         f"{i+1} ",
