349a28d74bb9564ffaa4f04f938248127f4d6c7a
kohenchia <kohenchia@gmail.com>
2024-06-21 08:06:50 -0700

    Add CORS for test server

--- tests/dev_api_server.py
+++ tests/dev_api_server.py
@@ -4,6 +4,7 @@ from typing import Optional, Dict
 
 # 3rd-party
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 # Local
@@ -21,6 +22,18 @@ else:
     LOG = logging.getLogger()
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/submissions")
