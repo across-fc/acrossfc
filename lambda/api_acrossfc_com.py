6b5fd9ecb84fb80f7559ce6a6ef3661bd40ace77
kohenchia <kohenchia@gmail.com>
2024-06-24 13:41:58 -0700

    Auth working

--- lambda/api_acrossfc_com.py
+++ lambda/api_acrossfc_com.py
@@ -2,6 +2,9 @@
 import os
 import re
 import json
+import requests
+import logging
+from typing import Optional, Dict
 from decimal import Decimal
 from collections.abc import MutableMapping, MutableSequence
 
@@ -9,7 +12,28 @@ from collections.abc import MutableMapping, MutableSequence
 from acrossfc import root_logger as LOG
 from acrossfc.api import submissions, participation_points, fc_roster
 from acrossfc.core.model import PointsCategory
+from acrossfc.core.config import FC_CONFIG
 from acrossfc.ext.ddb_client import DDB_CLIENT
+
+
+def response(status_code: int, msg: Optional[str] = None, data: Optional[Dict] = None):
+    headers = {
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+    }
+
+    body = msg
+
+    if data is not None:
+        body = json.dumps(convert_decimals_to_int(data))
+        headers['Content-Type'] = 'application/json'
+
+    return {
+        'statusCode': status_code,
+        'headers': headers,
+        'body': body
+    }
 
 
 def convert_decimals_to_int(obj):
@@ -27,48 +51,69 @@ def convert_decimals_to_int(obj):
 
 
 def lambda_handler(event, context):
+    LOG.setLevel(logging.INFO)
     raw_path = event['rawPath']
     PATH = raw_path.split('/')[1:]
     http_method = event['requestContext']['http']['method']
     qs_params = event.get('queryStringParameters', {})
-    resp_data = None
 
-    # TODO: Auth
+    # BEGIN Auth ------------------------
+    if 'x-ax-daccess-token' in event['headers']:
+        token = f"Bearer {event['headers']['x-ax-daccess-token']}"
+    elif 'x-ax-dbot-token' in event['headers']:
+        token = f"Bot {event['headers']['x-ax-dbot-token']}"
+    else:
+        return response(401)
 
+    d_resp = requests.get("https://discord.com/api/v10/users/@me", headers={
+        'Authorization': token
+    })
+
+    if d_resp.status_code != 200:
+        LOG.error(f"Error while trying to authorize with Discord. {d_resp.text}")
+        return response(401)
+
+    if d_resp['id'] not in FC_CONFIG.allowed_discord_id_list:
+        LOG.warn(f"Discord user {d_resp['name']} ({d_resp['id']}) tried to access the API.")
+        return response(403)
+    # END Auth ------------------------
 
     if PATH[0] == "fc_roster":
-        resp_data = fc_roster.get_fc_roster()
+        data = fc_roster.get_fc_roster()
+        return response(200, data=data)
 
     if PATH[0] == "current_tier":
-        resp_data = submissions.get_current_submissions_tier()
+        data = submissions.get_current_submissions_tier()
+        return response(200, data=data)
 
     if PATH[0] == "submissions":
         if http_method == 'GET':
             if len(PATH) == 1:
                 # DDB Scan, paginated
                 subs = submissions.get_submissions_for_tier(**qs_params)
-                resp_data = subs
+                return response(200, data=subs)
             elif PATH[1] == "queue":
                 queue = submissions.get_submissions_queue()
-                resp_data = queue
+                return response(200, data=queue)
             elif (m := re.fullmatch(r'^/submissions/([a-zA-Z0-9-]+)$', raw_path)):
                 sub_uuid = m.groups()[0]
                 sub = DDB_CLIENT.get_submission_by_uuid(sub_uuid)
-                resp_data = sub
+                return response(200, data=sub)
         elif http_method == 'POST':
             data_str = event.get('body', None)
             data = json.loads(data_str) if data_str is not None else {}
             if PATH[1] == "review":
-                resp_data = submissions.review_submission(**data)
+                data = submissions.review_submission(**data)
+                return response(200, data=data)
             elif PATH[1] == "evaluate":
                 eval_results = submissions.evaluate_fflogs(**data)
-                resp_data = eval_results
+                return response(200, data=eval_results)
             elif PATH[1] == "fflogs":
                 submissions.submit_fflogs(**data)
-                resp_data = {}
+                return response(200)
             elif PATH[1] == "manual":
                 submissions.submit_manual(**data)
-                resp_data = {}
+                return response(200)
 
     if PATH[0] == "ppts":
         if http_method == 'GET':
@@ -76,13 +121,16 @@ def lambda_handler(event, context):
                 if 'member_id' in qs_params:
                     # TODO: Figure out a cleaner solution
                     qs_params['member_id'] = int(qs_params['member_id'])
-                    resp_data = participation_points.get_points_for_member(**qs_params)
+                    data = participation_points.get_points_for_member(**qs_params)
+                    return response(200, data=data)
                 elif 'member_name' in qs_params:
-                    resp_data = participation_points.get_points_for_member_by_name(**qs_params)
+                    data = participation_points.get_points_for_member_by_name(**qs_params)
+                    return response(200, data=data)
             elif PATH[1] == "leaderboard":
-                resp_data = participation_points.get_points_leaderboard(**qs_params)
+                data = participation_points.get_points_leaderboard(**qs_params)
+                return response(200, data=data)
             elif PATH[1] == "table":
-                resp_data = [
+                data = [
                     {
                         'category_id': category.value,
                         'name': category.name,
@@ -92,24 +140,10 @@ def lambda_handler(event, context):
                     }
                     for category in PointsCategory
                 ]
+                return response(200, data=data)
 
-    if resp_data is None:
-        statusCode = 404
-        if os.environ.get('AX_ENV') == "TEST":
-            resp_data = event
-    else:
-        statusCode = 200
+    data = None
+    if os.environ.get('AX_ENV') == "TEST":
+        data = event
 
-    resp = {
-        'statusCode': statusCode,
-        'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        },
-        'body': json.dumps(convert_decimals_to_int(resp_data))
-    }
-
-    # In prod, return 404
-    return resp
+    return response(404, data=data)
